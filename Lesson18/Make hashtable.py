46bc2f11d8ff71dfc434ca61fa1fc595baccaa50
2nc <wnchit@163.com>
2019-04-20 18:40:19 +0800

    Make hashtable

---
+++ Lesson18/Make hashtable.py
@@ -0,0 +1,14 @@
+# Creating an Empty Hash Table
+# Define a procedure, make_hashtable,
+# that takes as input a number, nbuckets,
+# and returns an empty hash table with
+# nbuckets empty buckets.
+
+def make_hashtable(nbuckets):
+    index = []
+    i = 0
+    while i < nbuckets:
+        index.append([])
+        i = i + 1
+    return index
+
